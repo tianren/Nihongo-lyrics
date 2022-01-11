3ca34433b0db5c164ad524ff4b1123f1de16b56a
Tianren Liu <liutianren@gmail.com>
2022-01-11 23:40:10 +0800

    chocolate insomnia

--- Makefile
+++ Makefile
@@ -1,26 +1,38 @@
-compile_types = kanji-lyrics #dual-lyrics
+compile_type = kanji
+# compile_type = dual
+build_dir = $(compile_type)-build
+output_dir = $(compile_type)-lyrics
 
-ifneq ($(lyricstypemake),)
-lyrics = $(notdir $(wildcard ../source/*.tex))
-lyricspdf = $(lyrics:%.tex=%.pdf)
+lyrics = $(notdir $(wildcard source/*.tex))
+temppdf = $(lyrics:%.tex=$(build_dir)/%.pdf)
+lyricspdf = $(lyrics:%.tex=$(output_dir)/%.pdf)
+
+# lyricspdf = $(lyrics:%.tex=%.pdf)
 target: $(lyricspdf)
-$(lyricspdf): %.pdf: ../source/%.tex
-	# $(if $(shell head $< | grep -i "TeX program" | grep -io "LuaLaTeX"), lualatex --file-line-error $<, xelatex -shell-escape $<)
-	lualatex --file-line-error $<
-	# if [ -n "$(DISPLAY)" ]; then evince $@ & fi
-	if grep -q itemmark $<; then lualatex --file-line-error $<; fi
-else
-target: $(compile_types)
 
-$(compile_types):
-	export lyricstypemake=1 && make -C $@ --makefile=../Makefile
-endif
+$(lyricspdf): $(output_dir)/%.pdf: $(build_dir)/%.pdf
+	# mkdir -p $(@D)
+	cp -a $< $@
+
+$(temppdf): $(build_dir)/%.pdf: source/%.tex
+	# $(if $(shell head $< | grep -i "TeX program" | grep -io "LuaLaTeX"), lualatex --file-line-error $<, xelatex -shell-escape $<)
+	# mkdir -p $(@D)
+	mkdir -p $(build_dir)
+	cd $(build_dir) ; \
+	  lualatex --file-line-error ../$< ; \
+		if grep -q itemmark ../$<; then lualatex --file-line-error ../$<; fi;
+
+	# mv $(@:) $@
+	# if [ -n "$(DISPLAY)" ]; then evince $@ & fi
+	# if grep -q itemmark $<; then lualatex --file-line-error $<; fi
+
 
 debug:
 	@echo $(lyrics)
-	# @echo $(lyricspdf)
+	@echo $(lyricspdf)
+	@echo $(build_dir)
 
 clean:
-	find $(compile_types) \( -name *.log -o -name *.aux -o -name *.ltjruby \) -delete
+	find $(build_dir) \( -name *.log -o -name *.aux -o -name *.ltjruby \) -delete
 
-.PHONY: $(compile_types) debug target clean
+.PHONY: debug target clean
--- convert3.py
+++ convert3.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- encoding: UTF-8 -*-
 
 # convert html to lyrics from http://utaten.com
 
-import urllib2
+import urllib
 # import numpy as np
 # import re
 # import time, json
 import sys
 import difflib
 from bs4 import BeautifulSoup
-import StringIO, gzip
+from io import BytesIO
+import gzip
 
 useragent = [
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36",
@@ -20,30 +21,29 @@ header = {'User-Agent':useragent[0],
     'Accept':'text/html;q=0.9,*/*;q=0.8',
     'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
     'Accept-Encoding':'gzip',
-    'Connection':'close',
-    'Referer':'http://javtorrent.xyz/censored/111101/'}
+    'Connection':'close'}
 timeout = 5
 
 def urlopen(url):
-    request = urllib2.Request(url,None,header)
-    response = urllib2.urlopen(request,None,timeout)
+    request = urllib.request.Request(url,None,header)
+    response = urllib.request.urlopen(request,None,timeout)
     # if response.headers['content-encoding'] in ('gzip', 'x-gzip'):
-    data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(response.read()))
+    data = gzip.GzipFile('', 'rb', 9, BytesIO(response.read())).read()
     # elif response.headers['content-encoding'] == 'deflate':
     #     data = StringIO.StringIO(zlib.decompress(content))
     response.close()
-    return data
+    return data.decode("utf-8")
 
 def visitpage(href):
     # msp_href = 'http://javtorrent.xyz/date/2015/09/'
     try:
         page = urlopen(href)
-    except urllib2.HTTPError, e:
+    except urllib.error.HTTPError as e:
         return
     else:
         soup = BeautifulSoup(page, 'html.parser')
         m = soup.findChild(class_="lyricBody").findChild("div",class_="medium").findChild("div",class_="hiragana")
-        return m.encode_contents()
+        return m.encode_contents().decode('utf-8')
 
 prefix = r"""% !TeX encoding = UTF-8
 % !TeX program = LuaLaTeX
@@ -76,12 +76,13 @@ def tolatex(m):
         .replace('\n', "") \
         .replace('<br/><br/>',"\n  \\jisho{}\n\n\\item\n  ") \
         .replace(r'<br/>',"\n  \\jisho{}\n\n  ") \
+        .replace('<br>', "") \
         + suffix
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print "{} URL".format(sys.argv[0])
-        print "{} diff URL URL".format(sys.argv[0])
+        print ("{} URL".format(sys.argv[0]))
+        print ("{} diff URL URL".format(sys.argv[0]))
     elif sys.argv[1] == "diff":
         m1, m2 = (m.replace(r'</span></span>',"}")
             .replace(r'</span><span class="rt">',"{")
@@ -90,6 +91,6 @@ if __name__ == "__main__":
             .replace(r'<br/>',"\n")
             for m in (visitpage(sys.argv[2]), visitpage(sys.argv[3])))
         for line in difflib.context_diff(m1.decode('utf-8'), m2.decode('utf-8')):
-            print line
+            print (line)
     else:
-        print tolatex(visitpage(sys.argv[1]))
+        print (tolatex(visitpage(sys.argv[1])))
